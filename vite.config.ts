d3d4d1bc3f5cd6f5e319561f628562bab02daf5a
Yuchen Wang <contact@wangyuchen.cn>
2025-04-02 04:54:00 +0800

    chore: 更新项目配置和依赖
    - 在vite.config.ts中添加base路径配置
    - 更新package.json版本号至1.0.0并添加部署脚本
    - 添加gh-pages依赖以支持GitHub Pages部署

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from "@tailwindcss/vite"
-import path from "path"
+import path from "path";
+
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  base: "/package-json-visualizer/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
